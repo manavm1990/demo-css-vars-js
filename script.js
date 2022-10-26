ae14999bf8877f50a3a997841f8925cfaeaf725e
Manav Misra <manavm@visionwebsoft.com>
2022-10-25 19:08:26 -0500

    Select all buttons

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const h1 = document.querySelector("h1");
 const h2 = document.querySelector(".art-title");
+
+// TODO: Add click event listeners to all of the buttons
+const buttons = document.querySelectorAll("button");
